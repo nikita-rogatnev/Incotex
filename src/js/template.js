d4d6ee2f70e54c2d1942b3774607db7454abf95b
Rogatnev Nikita <mail@rogatnev.ru>
2016-12-24 13:34:30 +0800

    double range

--- src/js/template.js
+++ src/js/template.js
@@ -2,6 +2,73 @@ $(document)
 	.ready(function () {
 
 		// ------------------------------------------ //
+		// DOUBLE RANGE
+		// ------------------------------------------ //
+		var $range = $(".js-range-slider"),
+			$from = $(".js-from"),
+			$to = $(".js-to"),
+			range,
+			min = 0,
+			max = 2000,
+			from,
+			to;
+
+		var updateValues = function () {
+			$from.prop("value", from);
+			$to.prop("value", to);
+		};
+
+		$range.ionRangeSlider({
+			type: "double",
+			min: min,
+			max: max,
+			prettify_enabled: false,
+			grid: false,
+			decorate_both: false,
+			hide_min_max: true,
+			hide_from_to: true,
+
+			onChange: function (data) {
+				from = data.from;
+				to = data.to;
+
+				updateValues();
+			}
+		});
+
+		range = $range.data("ionRangeSlider");
+
+		var updateRange = function () {
+			range.update({from: from, to: to});
+		};
+
+		$from.on("change", function () {
+			from = +$(this).prop("value");
+			if (from < min) {
+				from = min;
+			}
+			if (from > to) {
+				from = to;
+			}
+
+			updateValues();
+			updateRange();
+		});
+
+		$to.on("change", function () {
+			to = +$(this).prop("value");
+			if (to > max) {
+				to = max;
+			}
+			if (to < from) {
+				to = from;
+			}
+
+			updateValues();
+			updateRange();
+		});
+
+		// ------------------------------------------ //
 		// HOME SLIDER
 		// ------------------------------------------ //
 		var slides = $('.slider__slide');
